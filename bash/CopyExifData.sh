13bde3d3b76d406a488f9f110bb87cd15d356cf8
florian <florian.diem@hotmail.com>
2021-02-24 14:39:02 +0100

    bugfix

--- bash/CopyExifData.sh
+++ bash/CopyExifData.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #use exif tool to copy all exif tags from mpo files to jpg files with the same name
+# $1 == jpgFilePrefix
 
 VAR1=$(dpkg -s libimage-exiftool-perl | grep Status)
 VAR2="Status: install ok installed"
@@ -8,13 +9,16 @@ if [ "$VAR1" = "$VAR2" ]; then
     jpgFilePrefix=$1 # jpg File prefix
 
     mpoFilesInDirectory=($(find -type f -name "*.MPO"))
-    jpgFilesInDirectory=($(find -type f -name $jpgFilePrefix"*.jpg"))
+    jpgFilesInDirectory=($(find -type f -name $jpgFilePrefix"*.JPG"))
+
+    echo $jpgFilePrefix
 
     OIFS=$IFS
 
     for mpoIndex in ${!mpoFilesInDirectory[*]}; 
     do
         mpoFile="${mpoFilesInDirectory[mpoIndex]}"
+        echo $mpoFile
         IFS='.' # hyphen (.) is set as delimiter
         read -ra ADDR <<< "$mpoFile" 
         IFS=$OIFS
@@ -23,6 +27,8 @@ if [ "$VAR1" = "$VAR2" ]; then
         for jpgIndex in ${!jpgFilesInDirectory[*]};
         do
             jpgFile="${jpgFilesInDirectory[jpgIndex]}"
+            echo $jpgFile
+
             if [[ $jpgFile == *$fileName* ]]; then
                 exiftool -TagsFromFile $mpoFile --Orientation $jpgFile
             fi
